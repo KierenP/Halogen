a09cfa04eaee509e4975795da913115907fcebae
Kieren Pearson <kierenpearson9@gmail.com>
2020-06-11 21:37:23 +1000

    No idea just trying more things

--- Halogen/src/build/cmake_install.cmake
+++ Halogen/src/build/cmake_install.cmake
@@ -32,11 +32,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/mingw64/bin/objdump.exe")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
