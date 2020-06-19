19ba4650ff033327eb0f406dd77842f12fa833c8
Kieren Pearson <kierenpearson9@gmail.com>
2020-06-19 15:02:30 +1000

    Changed version number
    Bench: 19210703

--- Halogen/src/main.cpp
+++ Halogen/src/main.cpp
@@ -10,7 +10,7 @@ uint64_t PerftDivide(unsigned int depth, Position& position);
 uint64_t Perft(unsigned int depth, Position& position);
 void Bench(Position& position);
 
-string version = "3.8.5"; 
+string version = "3.9"; 
 
 int main(int argc, char* argv[])
 {
