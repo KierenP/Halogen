e3dae33f302a5605463b7ffad43c6627bfc5d417
Kieren Pearson <kierenpearson9@gmail.com>
2020-07-14 01:12:11 +1000

    Final commit of version 5
    Bench: 12140859

--- Halogen/src/main.cpp
+++ Halogen/src/main.cpp
@@ -9,7 +9,7 @@ uint64_t PerftDivide(unsigned int depth, Position& position);
 uint64_t Perft(unsigned int depth, Position& position);
 void Bench(Position& position);
 
-string version = "4.5"; 
+string version = "5"; 
 
 int main(int argc, char* argv[])
 {
