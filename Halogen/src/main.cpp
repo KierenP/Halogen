2214d551811d0d08691aefd587e49d9990dcaa23
Kieren Pearson <kierenpearson9@gmail.com>
2020-08-08 23:13:02 +1000

    Change version format
    Bench: 7065054

--- Halogen/src/main.cpp
+++ Halogen/src/main.cpp
@@ -9,7 +9,7 @@ uint64_t PerftDivide(unsigned int depth, Position& position);
 uint64_t Perft(unsigned int depth, Position& position);
 void Bench();
 
-string version = "5.4"; 
+string version = "5-20200808-2310"; 
 
 int main(int argc, char* argv[])
 {
