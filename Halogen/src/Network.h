18e29a9fc4a7af35af4c0bdfdcd1a7032d00b9ab
Kieren Pearson <kierenpearson9@gmail.com>
2020-10-23 00:17:35 +1100

    First attempt at a quantized network

--- Halogen/src/Network.h
+++ Halogen/src/Network.h
@@ -12,15 +12,10 @@
 #include <cstring>
 #include "EvalCache.h"
 
-#define INPUT_NEURONS 12 * 64
+constexpr size_t INPUT_NEURONS = 12 * 64;
 
-struct trainingPoint
-{
-    trainingPoint(std::array<bool, INPUT_NEURONS> input, float gameResult);
-
-    std::array<bool, INPUT_NEURONS> inputs;
-    float result;
-};
+constexpr size_t MAX_VALUE = 128;
+constexpr size_t PRECISION = ((size_t)std::numeric_limits<int16_t>::max() + 1) / 128;
 
 struct deltaPoint
 {
@@ -30,35 +25,35 @@ struct deltaPoint
 
 struct Neuron
 {
-    Neuron(const std::vector<float>& Weight, float Bias);
-    float FeedForward(std::vector<float>& input, bool UseReLU) const;
+    Neuron(const std::vector<int16_t>& Weight, int16_t Bias);
+    int32_t FeedForward(std::vector<int16_t>& input, bool UseReLU) const;
 
-    std::vector<float> weights;
-    float bias;
+    std::vector<int16_t> weights;
+    int16_t bias;
 };
 
 struct HiddenLayer
 {
-    HiddenLayer(std::vector<float> inputs, size_t NeuronCount);    // <for first neuron>: weight1, weight2, ..., weightN, bias, <next neuron etc...>
-    std::vector<float> FeedForward(std::vector<float>& input, bool UseReLU);
+    HiddenLayer(std::vector<int16_t> inputs, size_t NeuronCount);    // <for first neuron>: weight1, weight2, ..., weightN, bias, <next neuron etc...>
+    std::vector<int16_t> FeedForward(std::vector<int16_t>& input, bool UseReLU);
 
     std::vector<Neuron> neurons;
     void ApplyDelta(std::vector<deltaPoint>& deltaVec);            //incrementally update the connections between input layer and first hidden layer
 
-    std::vector<float> zeta;
+    std::vector<int16_t> zeta;
 
 private:
-    std::vector<float> weightTranspose; //first neuron first weight, second neuron first weight etc...
+    std::vector<int16_t> weightTranspose; //first neuron first weight, second neuron first weight etc...
 };
 
 struct Network
 {
-    Network(std::vector<std::vector<float>> inputs, std::vector<size_t> NeuronCount);
-    float FeedForward(std::vector<float> inputs);
+    Network(std::vector<std::vector<int16_t>> inputs, std::vector<size_t> NeuronCount);
+    void RecalculateIncremental(std::vector<int16_t> inputs);
 
     void ApplyDelta(std::vector<deltaPoint>& delta);            //incrementally update the connections between input layer and first hidden layer
     void ApplyInverseDelta();     //for un-make moves
-    float QuickEval();                                                         //when used with above, this just calculates starting from the alpha of first hidden layer and skips input -> hidden
+    int16_t QuickEval();                                        //when used with above, this just calculates starting from the alpha of first hidden layer and skips input -> hidden
 
 private:
     size_t inputNeurons;
@@ -66,9 +61,9 @@ private:
     std::vector<HiddenLayer> hiddenLayers;
     Neuron outputNeuron;
 
-    float zeta;
+    int32_t zeta;
 
-    std::vector<std::vector<float>> OldZeta;
+    std::vector<std::vector<int16_t>> OldZeta;
     size_t incrementalDepth = 0;
 };
 
