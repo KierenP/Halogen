0a2bbaf2ea84eb1f4ac5a91a6fb45da3a73c5faa
Kieren Pearson <kierenpearson9@gmail.com>
2020-10-30 23:08:19 +1100

    Bench: 6111232

--- Halogen/src/Search.cpp
+++ Halogen/src/Search.cpp
@@ -366,7 +366,7 @@ SearchResult NegaScout(Position& position, unsigned int initialDepth, int depthR
 	}
 
 	/*Query the transpotition table*/
-	if (!IsPV(beta, alpha))
+	if (!IsPV(beta, alpha)) 
 	{
 		TTEntry entry = tTable.GetEntry(position.GetZobristKey());
 		if (CheckEntry(entry, position.GetZobristKey(), depthRemaining))
