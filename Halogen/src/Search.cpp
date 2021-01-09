69fe95e14796b80df6c757ccb163fc1613896f23
Kieren Pearson <kierenpearson9@gmail.com>
2021-01-10 01:43:11 +1100

    Bench: 24886981

--- Halogen/src/Search.cpp
+++ Halogen/src/Search.cpp
@@ -2,8 +2,8 @@
 
 /*Tuneable search constants*/
 
-double LMR_constant = -1.26;
-double LMR_coeff = 0.84;
+double LMR_constant = 0.5;
+double LMR_coeff = 0.6;
 
 int Null_constant = 4;
 int Null_depth_quotent = 6;
