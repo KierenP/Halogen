bddedfc074dcd6ef20125629891b0a7f737e0cf2
Kieren Pearson <kierenpearson9@gmail.com>
2021-02-02 07:30:20 +1100

    Bench: 24413714

--- Halogen/src/Network.cpp
+++ Halogen/src/Network.cpp
@@ -1,5 +1,5 @@
 #include "Network.h"
-#include "epoch5.net"
+#include "epoch50.net"
 
 std::array<std::array<int16_t, HIDDEN_NEURONS>, INPUT_NEURONS> Network::hiddenWeights = {};
 std::array<int16_t, HIDDEN_NEURONS> Network::hiddenBias = {};
