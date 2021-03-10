0d209e12e5b619e66ab9d3130d2da76b7fe79be5
Kieren Pearson <kierenpearson9@gmail.com>
2021-03-10 23:24:46 +1100

    Add unit tests for EvalNet.cpp (#276)
    Add unit tests for EvalNet
    Bench: 22944672

--- Halogen/src/EvalNet.cpp
+++ Halogen/src/EvalNet.cpp
@@ -1,13 +1,6 @@
 #include "EvalNet.h"
 
-int pieceValueVector[N_STAGES][N_PIECE_TYPES] = { {91, 532, 568, 715, 1279, 5000},
-                                                  {111, 339, 372, 638, 1301, 5000} };
-
-constexpr int TEMPO = 10;
-
-void NetworkScaleAdjustment(int& eval);
-void TempoAdjustment(int& eval, const Position& position);
-void NoPawnAdjustment(int& eval, const Position& position);
+using namespace UnitTestEvalNet;
 
 int EvaluatePositionNet(const Position& position, EvalCacheTable& evalTable)
 {
@@ -25,24 +18,6 @@ int EvaluatePositionNet(const Position& position, EvalCacheTable& evalTable)
     }
 
     return std::min(4000, std::max(-4000, eval));
-}
-
-void TempoAdjustment(int& eval, const Position& position)
-{
-    eval += position.GetTurn() == WHITE ? TEMPO : -TEMPO;
-}
-
-void NoPawnAdjustment(int& eval, const Position& position)
-{
-    if (eval > 0 && position.GetPieceBB(PAWN, WHITE) == 0)
-        eval /= 2;
-    if (eval < 0 && position.GetPieceBB(PAWN, BLACK) == 0)
-        eval /= 2;
-}
-
-void NetworkScaleAdjustment(int& eval)
-{
-    eval = eval * 94 / 100;
 }
 
 bool DeadPosition(const Position& position)
@@ -78,6 +53,28 @@ bool DeadPosition(const Position& position)
     return false;
 }
 
+UNIT_TEST_BEGIN(UnitTestEvalNet)
+
+void TempoAdjustment(int& eval, const Position& position)
+{
+    constexpr static int TEMPO = 10;
+    eval += position.GetTurn() == WHITE ? TEMPO : -TEMPO;
+}
+
+void NoPawnAdjustment(int& eval, const Position& position)
+{
+    if (eval > 0 && position.GetPieceBB(PAWN, WHITE) == 0)
+        eval /= 2;
+    if (eval < 0 && position.GetPieceBB(PAWN, BLACK) == 0)
+        eval /= 2;
+}
+
+void NetworkScaleAdjustment(int& eval)
+{
+    eval = eval * 94 / 100;
+}
+
+UNIT_TEST_END(UnitTestEvalNet);
 
 
 
