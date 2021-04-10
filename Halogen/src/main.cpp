76f973a5a02f015993ead2c603118073ad97ee0d
Kieren Pearson <kierenpearson9@gmail.com>
2021-04-10 20:58:17 +1000

    Update version number

--- Halogen/src/main.cpp
+++ Halogen/src/main.cpp
@@ -9,7 +9,7 @@ uint64_t PerftDivide(unsigned int depth, Position& position);
 uint64_t Perft(unsigned int depth, Position& position);
 void Bench(int depth = 16);
 
-string version = "10.9.1";
+string version = "10.9.2";
 
 int main(int argc, char* argv[])
 {
