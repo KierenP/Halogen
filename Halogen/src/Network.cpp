f3ad570097b1a0dac3847a9afe9f56dadd872b40
Kieren Pearson <kierenpearson9@gmail.com>
2021-06-23 16:19:42 +1000

    Bench: 25105552

--- Halogen/src/Network.cpp
+++ Halogen/src/Network.cpp
@@ -1,5 +1,7 @@
 #include "Network.h"
-#include "b8a1b7d5.nn"
+#include "incbin/incbin.h"
+
+INCBIN(Net, "4526ac9f.nn");
 
 std::array<std::array<int16_t, HIDDEN_NEURONS>, INPUT_NEURONS> Network::hiddenWeights = {};
 std::array<int16_t, HIDDEN_NEURONS> Network::hiddenBias = {};
@@ -31,7 +33,7 @@ void DotProduct(const std::array<T_in, SIZE>& a, const std::array<T_in, SIZE>& b
 
 void Network::Init()
 {
-    auto Data = reinterpret_cast<float*>(label);
+    auto Data = reinterpret_cast<const float*>(gNetData);
 
     for (size_t i = 0; i < HIDDEN_NEURONS; i++)
         hiddenBias[i] = (int16_t)round(*Data++ * PRECISION);
