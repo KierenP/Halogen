aa4a20ebac2a4fdf4737969c2f1f35b6cdccca25
KierenP <kierenpearson9@gmail.com>
2022-02-23 23:02:56 +1100

    Add new network by Luecx
    Bench: 28974643

--- src/Network.cpp
+++ src/Network.cpp
@@ -9,7 +9,7 @@
 #include "Position.h"
 #include "incbin/incbin.h"
 
-INCBIN(Net, "XXX.nn");
+INCBIN(Net, "169.nn");
 
 std::array<std::array<int16_t, HIDDEN_NEURONS>, INPUT_NEURONS> Network::hiddenWeights = {};
 std::array<int16_t, HIDDEN_NEURONS> Network::hiddenBias = {};
