6465303b2e4d58f1f4ead76ca7d59b2c0be054d0
Kieren Pearson <kierenpearson9@gmail.com>
2022-02-27 20:04:19 +1100

    Align TT entries with cache lines, use memset to avoid lazy loading of pages  (#372)
    Bench: 28974643

--- src/TTEntry.h
+++ src/TTEntry.h
@@ -7,7 +7,6 @@
 #include "Move.h"
 
 constexpr unsigned int HALF_MOVE_MODULO = 16;
-constexpr size_t BucketSize = 4;
 
 enum class EntryType : char
 {
@@ -24,13 +23,13 @@ public:
     TTEntry() = default;
     TTEntry(Move best, uint64_t ZobristKey, int Score, int Depth, int currentTurnCount, int distanceFromRoot, EntryType Cutoff);
 
-    bool IsAncient(unsigned int currentTurnCount, unsigned int distanceFromRoot) const { return halfmove != static_cast<char>((currentTurnCount - distanceFromRoot) % (HALF_MOVE_MODULO)); }
+    bool IsAncient(unsigned int currentTurnCount, unsigned int distanceFromRoot) const { return halfmove != CalculateAge(currentTurnCount, distanceFromRoot); }
 
     void SetHalfMove(int currentTurnCount, int distanceFromRoot) { halfmove = CalculateAge(currentTurnCount, distanceFromRoot); } //halfmove is from current position, distanceFromRoot adjusts this to get what the halfmove was at the root of the search
     void MateScoreAdjustment(int distanceFromRoot);
     void Reset();
 
-    static uint8_t CalculateAge(int currenthalfmove, int distanceFromRoot) { return (currenthalfmove - distanceFromRoot) % (HALF_MOVE_MODULO); }
+    static uint8_t CalculateAge(int currentTurnCount, int distanceFromRoot) { return (currentTurnCount - distanceFromRoot) % (HALF_MOVE_MODULO) + 1; }
 
     uint64_t GetKey() const { return key; }
     int GetScore() const { return score; }
@@ -43,16 +42,20 @@ private:
     /*Arranged to minimize padding*/
     uint64_t key; //8 bytes
     Move bestMove; //2 bytes
-    short int score; //2 bytes
-    char depth; //1 bytes
+    int16_t score; // 2 bytes
+    int8_t depth; // 1 bytes
     EntryType cutoff; //1 bytes
-    char halfmove; //1 bytes		(is stored as the halfmove at the ROOT of this current search, modulo 16)
+    int8_t halfmove; // 1 bytes	(is stored as the move count at the ROOT of this current search modulo 16 plus 1)
 };
 
 struct TTBucket
 {
+    constexpr static size_t SIZE = 4;
+
     void Reset();
-    std::array<TTEntry, BucketSize> entry;
+    alignas(64) std::array<TTEntry, SIZE> entry;
 };
 
+static_assert(sizeof(TTEntry) == 16, "TTEntry is not 16 bytes");
+static_assert(sizeof(TTBucket) == 64, "TTBucket is not 64 bytes");
 static_assert(std::is_trivial_v<TTBucket>);
