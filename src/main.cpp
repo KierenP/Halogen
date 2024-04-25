082b38889bda8ce5948bf36bf7b6ae0a0a48b9c3
Kieren Pearson <kierenpearson9@gmail.com>
2024-04-25 18:41:39 +1000

    Bump version number
    Bench: 25284700

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ uint64_t PerftDivide(unsigned int depth, GameState& position, bool chess960, boo
 uint64_t Perft(unsigned int depth, GameState& position, bool check_legality);
 void Bench(int depth = 14);
 
-string version = "11.7.2";
+string version = "11.7.3";
 
 int main(int argc, char* argv[])
 {
