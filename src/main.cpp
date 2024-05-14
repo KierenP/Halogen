0d7fc3c193f80c39815f2475163b282baf0ec702
Kieren Pearson <kierenpearson9@gmail.com>
2024-05-14 20:36:19 +1000

    Bench: 3967077

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ uint64_t PerftDivide(unsigned int depth, GameState& position, bool chess960, boo
 uint64_t Perft(unsigned int depth, GameState& position, bool check_legality);
 void Bench(int depth = 10);
 
-string version = "11.17.0";
+string version = "11.18.0";
 
 int main(int argc, char* argv[])
 {
