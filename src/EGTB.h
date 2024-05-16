842f83911c6ee63e3b0074a82b65e68baeb6560c
Kieren Pearson <kierenpearson9@gmail.com>
2024-05-16 18:25:43 +1000

    Bench: 3967077

--- src/EGTB.h
+++ src/EGTB.h
@@ -8,24 +8,9 @@
 #include <optional>
 #include <string_view>
 
-// models a Pyrrhic TB_RESULT and converts move/score to our domain types
-class TbResult
-{
-public:
-    TbResult() {};
-    TbResult(uint32_t result);
-
-    Score get_score(int distance_from_root) const;
-    Move get_move(const BoardState& board) const;
-
-private:
-    uint32_t result_;
-};
-
 class RootProbeResult
 {
 public:
-    TbResult root_result_;
     BasicMoveList root_move_whitelist;
 };
 
@@ -34,6 +19,6 @@ class Syzygy
 public:
     static void init(std::string_view path);
 
-    static std::optional<TbResult> probe_wdl_search(const BoardState& board);
+    static std::optional<Score> probe_wdl_search(const BoardState& board, int distance_from_root);
     static std::optional<RootProbeResult> probe_dtz_root(const BoardState& board);
 };
