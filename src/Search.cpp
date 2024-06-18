91de12027717a8256305afb680e3170c96000741
Kieren Pearson <kierenpearson9@gmail.com>
2024-06-18 22:47:17 +1000

    Bench: 3163707

--- src/Search.cpp
+++ src/Search.cpp
@@ -693,7 +693,7 @@ SearchResult NegaScout(GameState& position, SearchStackState* ss, SearchLocalSta
 
     const auto staticScore = EvaluatePositionNet(position, local.eval_cache);
 
-    if (staticScore < alpha - 500 - 300 * depth * depth)
+    if (!pv_node && staticScore < alpha - 500 - 300 * depth * depth)
     {
         auto q_score = Quiescence<SearchType::ZW>(position, ss, local, shared, depth, alpha - 1, alpha).GetScore();
         if (q_score < alpha)
