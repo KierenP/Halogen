bb9bcf8ad1cd4512fe1c3f9ba9133c845497e04e
Kieren Pearson <kierenpearson9@gmail.com>
2024-09-15 14:13:46 +1000

    Bench: 1204041

--- src/Search.cpp
+++ src/Search.cpp
@@ -826,11 +826,12 @@ SearchResult NegaScout(GameState& position, SearchStackState* ss, SearchLocalSta
             }
         }
 
-        int history = move.IsCapture() || move.IsPromotion() ? local.loud_history.get(position, ss, move)
-                                                             : local.quiet_history.get(position, ss, move);
+        bool is_loud_move = move.IsCapture() || move.IsPromotion();
+        int history
+            = is_loud_move ? local.loud_history.get(position, ss, move) : local.quiet_history.get(position, ss, move);
 
-        if (score > Score::tb_loss_in(MAX_DEPTH) && !move.IsCapture() && !move.IsPromotion() && depth <= 10
-            && !see_ge(position.Board(), move, -20 * depth * depth - history / 128))
+        if (score > Score::tb_loss_in(MAX_DEPTH) && !is_loud_move && depth <= 10
+            && !see_ge(position.Board(), move, -64 * depth - history / 128))
         {
             continue;
         }
