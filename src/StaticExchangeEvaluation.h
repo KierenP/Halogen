eebddb797bdee1b2434c763ba75a26ea6801edee
Kieren Pearson <kierenpearson9@gmail.com>
2025-04-20 00:05:19 +1000

    Bench: 1187845

--- src/StaticExchangeEvaluation.h
+++ src/StaticExchangeEvaluation.h
@@ -4,7 +4,7 @@ class BoardState;
 class Move;
 class Score;
 
-constexpr int PieceValues[] = { 91, 532, 568, 715, 1279, 5000, 91, 532, 568, 715, 1279, 5000 };
+constexpr int PieceValues[] = { 91, 532, 568, 715, 1279, 5000, 91, 532, 568, 715, 1279, 5000, 0 };
 
 int see(const BoardState& board, Move move);
 bool see_ge(const BoardState& board, Move move, Score threshold);
