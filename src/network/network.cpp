69dcddf33773049494b7ddde2d948f75ed834ac7
Kieren Pearson <kierenpearson9@gmail.com>
2025-05-17 00:20:22 +1000

    Bench: 1257968

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -29,7 +29,7 @@ struct raw_network
 
 [[maybe_unused]] auto verify_network_size = []
 {
-    assert(sizeof(network) == gNetSize);
+    assert(sizeof(raw_network) == gNetSize);
     return true;
 }();
 
