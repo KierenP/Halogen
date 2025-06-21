04396044bfc326754e628ac50ba5bcc581214c76
Kieren Pearson <kierenpearson9@gmail.com>
2025-06-21 16:50:23 +1000

    Change script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 for i in $(seq 1 $(nproc --all));
 do
-    nice -19 taskset -c $[i - 1] ./bin/Halogen-native "setoption name SyzygyPath value $1" "datagen output $i duration $2" > $i.log &
+    nice -19 taskset -c $[i - 1] ./Halogen-native "setoption name SyzygyPath value $1" "datagen output $i duration $2" > $i.log &
 done
