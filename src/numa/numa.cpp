3b5f668220f9efca0c33f174029414814eb28556
Kieren Pearson <kierenpearson9@gmail.com>
2025-07-13 01:45:54 +1000

    Bench: 1260610

--- src/numa/numa.cpp
+++ src/numa/numa.cpp
@@ -1,23 +1,59 @@
 #include "numa.h"
 #include <cstdlib>
+#include <vector>
+
+#define TOURNAMENT_MODE
 
 #ifdef TOURNAMENT_MODE
 #include <iostream>
 #include <numa.h>
 
-void bind_search_thread(int core)
+std::vector<cpu_set_t> get_cpu_masks_per_numa_node()
 {
-    int node = numa_node_of_cpu(core);
-
-    if (node < 0)
+    if (numa_available() == -1)
     {
-        std::cerr << "Failed to get NUMA node for core " << core << std::endl;
+        std::cerr << "NUMA is not available on this system" << std::endl;
         std::exit(EXIT_FAILURE);
     }
 
-    struct bitmask* mask = numa_allocate_nodemask();
-    numa_bitmask_setbit(mask, node);
-    numa_bind(mask);
+    const int max_node = numa_max_node();
+
+    std::vector<cpu_set_t> node_cpu_masks;
+
+    for (int node = 0; node <= max_node; ++node)
+    {
+        struct bitmask* cpumask = numa_allocate_cpumask();
+
+        if (numa_node_to_cpus(node, cpumask) != 0)
+        {
+            std::cerr << "Failed to get CPUs for NUMA node: " << node << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+
+        for (size_t cpu = 0; cpu < cpumask->size; ++cpu)
+        {
+            if (numa_bitmask_isbitset(cpumask, cpu))
+            {
+                CPU_SET(cpu, &cpuset);
+            }
+        }
+
+        numa_free_cpumask(cpumask);
+        node_cpu_masks.push_back(cpuset);
+    }
+
+    return node_cpu_masks;
+}
+
+void numa_bind_thread(size_t index)
+{
+    static auto mapping = get_cpu_masks_per_numa_node();
+    size_t node = index % mapping.size();
+    pthread_t handle = pthread_self();
+    pthread_setaffinity_np(handle, sizeof(cpu_set_t), &mapping[node]);
 }
 
 #else
