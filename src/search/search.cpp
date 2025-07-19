8278bc28dee951f28f13390ff322e7077a317c02
Kieren Pearson <kierenpearson9@gmail.com>
2025-07-19 23:55:55 +1000

    Bench: 1301336

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -791,7 +791,7 @@ Score search(GameState& position, SearchStackState* ss, SearchLocalState& local,
     // If our static store is above beta, we skip a move. If the resulting position is still above beta, then we can
     // fail high assuming there is at least one move in the current position that would allow us to improve. This
     // heruistic fails in zugzwang positions, so we have a verification search.
-    if (!pv_node && !InCheck && ss->singular_exclusion == Move::Uninitialized && (ss - 1)->move != Move::Uninitialized
+    if (!ttpv && !InCheck && ss->singular_exclusion == Move::Uninitialized && (ss - 1)->move != Move::Uninitialized
         && distance_from_root >= ss->nmp_verification_depth && eval > beta
         && !(tt_entry && tt_cutoff == SearchResultType::UPPER_BOUND && tt_score < beta))
     {
