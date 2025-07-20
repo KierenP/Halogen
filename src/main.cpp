fb1c88bce082ccc49c065238a6a8ed294a979f28
Kieren Pearson <kierenpearson9@gmail.com>
2025-07-20 22:49:01 +1000

    Bench: 1180739

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "test/static_exchange_evaluation_test.h"
 #include "uci/uci.h"
 
-constexpr std::string_view version = "13.12.2";
+constexpr std::string_view version = "13.12.3";
 
 void PrintVersion()
 {
