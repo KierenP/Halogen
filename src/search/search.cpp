aa8020dd385c28c7491a108ce3f8eb1ab3000f4d
Kieren Pearson <kierenpearson9@gmail.com>
2025-08-02 19:21:40 +1000

    Bench: 1261870

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -807,7 +807,7 @@ Score search(GameState& position, SearchStackState* ss, SearchLocalState& local,
     }
 
     const Score prob_cut_beta = beta + 100;
-    if (!pv_node && depth >= 3)
+    if (!pv_node && depth >= 3 && eval > beta)
     {
         StagedMoveGenerator probcut_gen
             = StagedMoveGenerator::probcut(position, ss, local, tt_move, prob_cut_beta - eval);
