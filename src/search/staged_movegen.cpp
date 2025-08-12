acd93aa0801ad0edbf545d19d6cf4c6336f7042e
Kieren Pearson <kierenpearson9@gmail.com>
2025-08-12 23:21:33 +1000

    Bench: 7218115

--- src/search/staged_movegen.cpp
+++ src/search/staged_movegen.cpp
@@ -61,7 +61,7 @@ bool StagedMoveGenerator::next(Move& move)
     {
         while (current != loudMoves.end())
         {
-            if (see_ge(position.board(), current->move, 100))
+            if (see_ge(position.board(), current->move, -50))
             {
                 move = current->move;
                 ++current;
