86d6ab59acd25434c05783edde55f896f73045ac
Kieren Pearson <kierenpearson9@gmail.com>
2025-10-06 00:14:50 +1100

    Bench: 3836025

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -810,7 +810,7 @@ Score search(GameState& position, SearchStackState* ss, NN::Accumulator* acc, Se
     if ((ss - 1)->move != Move::Uninitialized && !position.prev_board().checkers && !(ss - 1)->move.is_capture()
         && !InCheck)
     {
-        int bonus = std::clamp(-10 * (ss->adjusted_eval + (ss - 1)->adjusted_eval).value() + 500, -1000, 1000);
+        int bonus = std::clamp((-10 * (ss->adjusted_eval + (ss - 1)->adjusted_eval).value() + 500) / 64, -16, 16);
         local.pawn_hist.add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
         local.threat_hist.add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
         if ((ss - 2)->cont_hist_subtable)
