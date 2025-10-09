997bf24933a2cc71bdb79ed3a002e8158ae8b861
Kieren Pearson <kierenpearson9@gmail.com>
2025-10-09 23:16:30 +1100

    Bench: 9098856

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -468,7 +468,7 @@ std::optional<Score> singular_extensions(GameState& position, SearchStackState* 
     ss->singular_exclusion = Move::Uninitialized;
 
     // If the TT move is singular, we extend the search by one or more plies depending on how singular it appears
-    if (se_score < sbeta - se_double && !pv_node)
+    if (se_score < sbeta - se_double)
     {
         extensions += 2;
     }
