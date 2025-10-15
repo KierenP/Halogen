55d296681590e173cb401872f0e60832133957d7
Kieren Pearson <kierenpearson9@gmail.com>
2025-10-15 22:21:54 +1100

    Bench: 5267813

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -1102,6 +1102,8 @@ Score search(GameState& position, SearchStackState* ss, NN::Accumulator* acc, Se
 
         if (root_node)
         {
+            std::cout << "searching move " << seen_moves << " " << move << " with score " << search_score << std::endl;
+
             auto& root_move = *std::ranges::find(local.root_moves, move, &RootMove::move);
             root_move.nodes += local.nodes - prev_nodes;
             root_move.search_depth = local.curr_depth;
