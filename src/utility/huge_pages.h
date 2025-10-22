f687ba3332ff0aded33e3a52cb229c8e11b20ee5
Kieren Pearson <kierenpearson9@gmail.com>
2025-10-22 11:07:12 +1100

    Bench: 4373167

--- src/utility/huge_pages.h
+++ src/utility/huge_pages.h
@@ -79,10 +79,10 @@ T* allocate_huge_page(std::size_t size)
         }
 
         LPSTR messageBuffer = nullptr;
-        size_t size = FormatMessageA(
+        size_t str_size = FormatMessageA(
             FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
             errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-        std::string message(messageBuffer, size);
+        std::string message(messageBuffer, str_size);
 
         std::cerr << "Failed to allocate huge page: " << messageBuffer << std::endl;
         std::terminate();
