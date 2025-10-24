052b97b440e008c34775d233f9b34ef4f7888a85
Kieren Pearson <kierenpearson9@gmail.com>
2025-10-25 00:37:17 +1100

    Bench: 4574873

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -936,15 +936,8 @@ Score search(GameState& position, SearchStackState* ss, NN::Accumulator* acc, Se
     if ((ss - 1)->move != Move::Uninitialized && !position.prev_board().checkers && !(ss - 1)->move.is_capture()
         && !InCheck)
     {
-        int bonus = std::clamp((-10 * (ss->adjusted_eval + (ss - 1)->adjusted_eval).value() + 500) / 64, -16, 16);
-        // local.pawn_hist.add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
+        int bonus = std::clamp((-10 * (ss->adjusted_eval + (ss - 1)->adjusted_eval).value() + 1150) / 256, -16, 16);
         local.threat_hist.add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
-        /*if ((ss - 2)->cont_hist_subtable)
-            (ss - 2)->cont_hist_subtable->add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
-        if ((ss - 3)->cont_hist_subtable)
-            (ss - 3)->cont_hist_subtable->add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
-        if ((ss - 5)->cont_hist_subtable)
-            (ss - 5)->cont_hist_subtable->add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);*/
     }
 
     // Step 9: Rebel style IID
