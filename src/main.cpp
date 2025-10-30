866851a3446327c515de44fa4dfccb6434069bf3
Kieren Pearson <kierenpearson9@gmail.com>
2025-10-30 17:20:35 +1100

    Use mailbox representation in more places (#783)
    Bench: 4373167

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "test/static_exchange_evaluation_test.h"
 #include "uci/uci.h"
 
-constexpr std::string_view version = "15.12.1";
+constexpr std::string_view version = "15.12.2";
 
 void PrintVersion()
 {
