8e9136aac3c79d3a2ec5cca25c56061eed0e8f29
Kieren Pearson <kierenpearson9@gmail.com>
2025-11-22 23:39:38 +1100

    Bench: 3851607

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -1232,6 +1232,19 @@ Score search(GameState& position, SearchStackState* ss, NN::Accumulator* acc, Se
     {
         const auto bonus = pcm_const + pcm_depth * depth + pcm_quad * depth * depth;
         local.threat_hist.add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
+        local.pawn_hist.add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
+        if ((ss - 2)->cont_hist_subtable)
+        {
+            (ss - 2)->cont_hist_subtable->add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
+        }
+        if ((ss - 3)->cont_hist_subtable)
+        {
+            (ss - 3)->cont_hist_subtable->add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
+        }
+        if ((ss - 5)->cont_hist_subtable)
+        {
+            (ss - 5)->cont_hist_subtable->add(position.prev_board(), ss - 1, (ss - 1)->move, bonus);
+        }
     }
 
     const auto bound = score <= original_alpha ? SearchResultType::UPPER_BOUND
