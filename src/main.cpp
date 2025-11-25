2ea30c90119201a61d793b67b1b1b9cc72033d9c
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-11-25 12:10:06 +0300

    Do Futility Pruning in pv nodes (#830)
    Bench: 3988903

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "15.21.0";
+constexpr std::string_view version = "15.22.0";
 
 int main(int argc, char* argv[])
 {
