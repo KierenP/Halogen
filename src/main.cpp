a2463e7513fccecf11ce5a52386e8a2dad99a67e
Kieren Pearson <kierenpearson9@gmail.com>
2025-12-07 16:41:15 +1100

    Allow negative LMR reduction for PV nodes (#838)
    Bench: 4560817

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "15.25.0";
+constexpr std::string_view version = "15.26.0";
 
 int main(int argc, char* argv[])
 {
