98b6aff740dfeb95e826f1feba4eb7ea39e5a5e6
Kieren Pearson <kierenpearson9@gmail.com>
2026-02-12 21:37:43 +1100

    Update default network to 7b44b83b.nn (#862)
    Bench: 4392658

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "16.0.0";
+constexpr std::string_view version = "16.1.0";
 
 int main(int argc, char* argv[])
 {
