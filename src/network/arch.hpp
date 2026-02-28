335a37d992177dce1c807a96d3ef7929c4841e87
Kieren Pearson <kierenpearson9@gmail.com>
2026-02-28 17:05:52 +1100

    Bench: 3717720

--- src/network/arch.hpp
+++ src/network/arch.hpp
@@ -15,7 +15,7 @@ namespace NN
 
 constexpr size_t INPUT_SIZE = 12 * 64;
 constexpr size_t FT_SIZE = 640;
-constexpr size_t L1_SIZE = 16;
+constexpr size_t L1_SIZE = 32;
 constexpr size_t L2_SIZE = 32;
 
 constexpr size_t OUTPUT_BUCKETS = 8;
