e3d360d11fa7fefb2fcf711df22ee58506c98d93
Kieren Pearson <kierenpearson9@gmail.com>
2026-03-04 22:43:21 +1100

    Expand list of bench positions (#869)
    Bench: 5923615

--- src/misc/benchmark.h
+++ src/misc/benchmark.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include <array>
-#include <string_view>
 
 // copied from stockfish
 // clang-format off
-const std::array<std::string_view, 36> benchMarkPositions = {
+const std::array benchMarkPositions = {
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
     "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 10",
     "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 11",
@@ -41,6 +40,32 @@ const std::array<std::string_view, 36> benchMarkPositions = {
     "r3k2r/3nnpbp/q2pp1p1/p7/Pp1PPPP1/4BNN1/1P5P/R2Q1RK1 w kq - 0 16",
     "3Qb1k1/1r2ppb1/pN1n2q1/Pp1Pp1Pr/4P2p/4BP2/4B1R1/1R5K b - - 11 40",
     "4k3/3q1r2/1N2r1b1/3ppN2/2nPP3/1B1R2n1/2R1Q3/3K4 w - - 5 1",
-    "5k2/8/3PK3/8/8/8/8/8 w - - 0 1",
+
+    // Positions with high numbers of changed threats
+    "k7/2n1n3/1nbNbn2/2NbRBn1/1nbRQR2/2NBRBN1/3N1N2/7K w - - 0 1",
+    "K7/8/8/BNQNQNB1/N5N1/R1Q1q2r/n5n1/bnqnqnbk w - - 0 1",
+
+    // 5-man positions
+    "8/8/8/8/5kp1/P7/8/1K1N4 w - - 0 1",     // Kc2 - mate
+    "8/8/8/5N2/8/p7/8/2NK3k w - - 0 1",      // Na2 - mate
+    "8/3k4/8/8/8/4B3/4KB2/2B5 w - - 0 1",    // draw
+
+    // 6-man positions
+    "8/8/1P6/5pr1/8/4R3/7k/2K5 w - - 0 1",   // Re5 - mate
+    "8/2p4P/8/kr6/6R1/8/8/1K6 w - - 0 1",    // Ka2 - mate
+    "8/8/3P3k/8/1p6/8/1P6/1K3n2 b - - 0 1",  // Nd2 - draw
+
+    // 7-man positions
+    "8/R7/2q5/8/6k1/8/1P5p/K6R w - - 0 124", // Draw
+
+    // Mate and stalemate positions
+    "6k1/3b3r/1p1p4/p1n2p2/1PPNpP1q/P3Q1p1/1R1RB1P1/5K2 b - - 0 1",
+    "r2r1n2/pp2bk2/2p1p2p/3q4/3PN1QP/2P3R1/P4PP1/5RK1 w - - 0 1",
+    //"8/8/8/8/8/6k1/6p1/6K1 w - -", // Halogen doesn't accept positions where the root is a stalemate
+    //"7k/7P/6K1/8/3B4/8/8/8 b - -", // Halogen doesn't accept positions where the root is a checkmate
+
+    // Chess 960
+    "bbqnnrkr/pppppppp/8/8/8/8/PPPPPPPP/BBQNNRKR w HFhf - 0 1 moves g2g3 d7d5 d2d4 c8h3 c1g5 e8d6 g5e7 f7f6",
+    "nqbnrkrb/pppppppp/8/8/8/8/PPPPPPPP/NQBNRKRB w KQkq - 0 1",
 };
 // clang-format on
