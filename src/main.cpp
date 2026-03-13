a8e9bcbf7d19ea8014ec97ef83d9b07d066035c1
Kieren Pearson <kierenpearson9@gmail.com>
2026-03-13 23:30:57 +1100

    Bench: 6223974

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "16.3.2";
+constexpr std::string_view version = "16.4.0";
 
 int main(int argc, char* argv[])
 {
