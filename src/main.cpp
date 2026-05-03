b95a49e106f2f8d95491f8199a3c450a15040549
Kieren Pearson <kierenpearson9@gmail.com>
2026-05-03 19:53:37 +1000

    Load L2 bias directly into register (#874)
    Bench: 5644257

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "16.4.3";
+constexpr std::string_view version = "16.4.4";
 
 int main(int argc, char* argv[])
 {
