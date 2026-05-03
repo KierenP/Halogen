e8898483c321e0d667a63004cf09a90ac6d6b8f7
Kieren Pearson <kierenpearson9@gmail.com>
2026-05-03 21:41:44 +1000

    On Avx512, maintain 4x independent dpbusd in flight (#876)
    Bench: 5644257

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "16.4.4";
+constexpr std::string_view version = "16.4.5";
 
 int main(int argc, char* argv[])
 {
