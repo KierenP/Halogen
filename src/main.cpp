e98b2ab2104bee8314e8301e69ba26f536cd3485
Kieren Pearson <kierenpearson9@gmail.com>
2026-07-01 22:26:18 +1000

    Upgrade default network to df9e042a.nn (#885)
    Bench: 5409018

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "uci/uci.h"
 #include "utility/arch.h"
 
-constexpr std::string_view version = "16.5.3";
+constexpr std::string_view version = "16.6.0";
 
 int main(int argc, char* argv[])
 {
